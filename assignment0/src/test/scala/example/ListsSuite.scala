7c894c313b5caedead6a92f566d00ee812a77b29
dmacdonald2013 <davidm@cdonald.net.nz>
2014-09-18 18:01:34 +1200

    completed first assignment

--- assignment0/src/test/scala/example/ListsSuite.scala
+++ assignment0/src/test/scala/example/ListsSuite.scala
@@ -4,6 +4,7 @@ import org.scalatest.FunSuite
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import java.util.NoSuchElementException
 
 /**
  * This class implements a ScalaTest test suite for the methods in object
@@ -47,7 +48,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +73,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2) // Fix me, please!
   }
 
 
@@ -121,4 +122,27 @@ class ListsSuite extends FunSuite {
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
   }
+
+  test("max empty list") {
+    intercept[NoSuchElementException] {
+      max(List())
+    }
+  }
+
+  test("max of one number") {
+    assert(max(List(7)) === 7)
+  }
+
+  test("sum of no numbers is 0") {
+    assert(sum(List()) === 0)
+  }
+
+  test("sum of one number") {
+    assert(sum(List(5)) === 5)
+  }
+
+  test("max of two numbers") {
+    assert(max(List(7,5)) === 7)
+  }
+
 }
