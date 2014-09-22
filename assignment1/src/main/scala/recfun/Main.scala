5f45c8ef2044f3aa85aefe9d1082475eb25f1821
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2014-09-22 20:46:30 +1200

    completed exercises 1 & 2

--- assignment1/src/main/scala/recfun/Main.scala
+++ assignment1/src/main/scala/recfun/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -14,12 +16,24 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = if (c == 0 || c == r) 1 else pascal(c-1,r-1) + pascal(c,r-1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def openCount(chars: List[Char], open: Int): Int = {
+      if (chars.isEmpty) open
+      else chars.head match {
+        case ')'  => if (open == 0) 1 else openCount(chars.tail,open-1)
+        case '('  => openCount(chars.tail,open+1)
+        case _    => openCount(chars.tail,open)
+      }
+    }
+    openCount(chars,0) == 0
+  }
+
 
   /**
    * Exercise 3
