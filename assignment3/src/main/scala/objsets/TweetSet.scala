ee88e4b51b2672b8414e9ded9c91de8dc11207ee
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2014-10-04 20:55:25 +1300

    finished first 2 exercises

--- assignment3/src/main/scala/objsets/TweetSet.scala
+++ assignment3/src/main/scala/objsets/TweetSet.scala
@@ -42,11 +42,8 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet
 
-  /**
-   * This is a helper method for `filter` that propagetes the accumulated tweets.
-   */
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
 
   /**
@@ -55,7 +52,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-   def union(that: TweetSet): TweetSet = ???
+   def union(that: TweetSet): TweetSet
 
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -110,7 +107,11 @@ abstract class TweetSet {
 
 class Empty extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = this
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
+  def union(that: TweetSet): TweetSet = that
 
 
   /**
@@ -128,8 +129,19 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
 
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+    val lacc = left.filterAcc(p,acc)
+    val racc = right.filterAcc(p,lacc)
+    if (p(elem)) racc.incl(elem)
+    else racc
+  }
+
+  def union(that: TweetSet): TweetSet = {
+    new NonEmpty(elem, left.union(that.filter((t) => t.text < elem.text)),
+      right.union(that.filter((t) => elem.text < t.text)))
+  }
 
   /**
    * The following methods are already implemented
